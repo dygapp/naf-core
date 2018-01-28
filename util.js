fcca6092d4b4aab1f98701abe61f247eb6dde65f
dygapp <dygapp@qq.com>
2018-01-29 01:14:32 +0800

    0.0.5

--- util.js
+++ util.js
@@ -2,9 +2,9 @@
 
 const is = require('is-type-of');
 
-module.exports.trimData = data => {
+module.exports.trimData = (data, exclude = []) => {
   for (const key in data) {
-    if (is.undefined(data[key])) delete data[key];
+    if (is.undefined(data[key]) || exclude.indexOf(key) !== -1) delete data[key];
   }
   return data;
 };
