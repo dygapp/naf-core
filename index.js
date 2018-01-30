fa180b5938dd9cb9a3784488c36bf504cd662bc1
dygapp <dygapp@qq.com>
2018-01-30 19:42:22 +0800

    0.0.10

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const Error = require('./lib/naf-error')
+const Error = require('./lib/naf-error');
 const { ErrorCode } = require('./lib/error-code');
+const Util = require('./lib/naf-util')
 
 module.exports = {
   Error,
   ErrorCode,
+  Util,
 };
