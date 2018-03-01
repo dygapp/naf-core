b412fccbb513be84d14683a7a00224a8650dffb2
dygapp <dygapp@qq.com>
2018-03-01 16:17:57 +0800

    0.0.19 完善类型定义

--- index.d.ts
+++ index.d.ts
@@ -24,4 +24,34 @@ export class Util {
   
 }
 
+declare namespace Error {
+
+  export class NafError {
+    constructor(err, message, details);
+  }
+  
+  export class BusinessError extends NafError {
+    static getErrorMsg(errcode);
+  }
+
+  declare namespace ErrorCode {
+    declare var UNKNOW: number;
+    declare var BADPARAM: number;
+    declare var NETWORK: number;
+    declare var JSON_ERROR: number;
+    declare var USER_NOT_EXIST: number;
+    declare var BAD_PASSWORD: number;
+    declare var NOT_LOGIN: number;
+    declare var ACCESS_DENIED: number;
+    declare var DATA_NOT_EXIST: number;
+    declare var DATA_EXISTED: number;
+    declare var DATA_INVALID: number;
+    declare var VERIFYCODE_INVALID: number;
+    declare var SERVICE_FAULT: number;
+    declare var DATABASE_FAULT: number;
+    declare var FILE_FAULT: number;
+    declare var BUSINESS: number;
+  }
+}
+
 export as namespace NafCore;
