1f27d81cf2a44bf9f8ff4bc6d17a42f3db9437be
dygapp <dygapp@qq.com>
2018-08-30 15:58:31 +0800

    use lodash

--- index.d.ts
+++ index.d.ts
@@ -6,22 +6,7 @@ export class Util {
 
   static isMobile(val: string): boolean;
 
-  // from core-util-is
-  static isArray(arg);  
-  static isBoolean(arg);  
-  static isNull(arg);  
-  static isNullOrUndefined(arg);  
-  static isNumber(arg);  
-  static isString(arg);  
-  static isSymbol(arg);  
-  static isUndefined(arg);  
-  static isRegExp(re);  
-  static isObject(arg);  
-  static isDate(d);  
-  static isError(e);  
-  static isstatic(arg);  
-  static isPrimitive(arg);
-  
+  static isNullOrUndefined(val: any): boolean;
 }
 
 declare namespace Error {
